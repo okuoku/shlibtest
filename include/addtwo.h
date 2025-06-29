9400a42cb455c9cb1a9b347abf748b5dd0557309
okuoku <mjt@cltn.org>
2025-06-29 14:17:25 +0900

    elf: Control visibility

--- include/addtwo.h
+++ include/addtwo.h
@@ -6,12 +6,16 @@
 #if defined(__CYGWIN__) || defined(_WIN32)
 // https://learn.microsoft.com/en-us/cpp/build/exporting-from-a-dll-using-declspec-dllexport
 #define MODEXPORT __declspec(dllexport)
+#else /* ELF platform */
+#define MODEXPORT __attribute__((visibility ("default")))
 #endif
 #else
 /* Define MODEXPORT = import */
 #if defined(__CYGWIN__) || defined(_WIN32)
 // https://learn.microsoft.com/en-us/cpp/build/importing-into-an-application-using-declspec-dllimport
 #define MODEXPORT __declspec(dllimport)
+#else /* ELF platform */
+#define MODEXPORT
 #endif
 #endif
 
